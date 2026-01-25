7e186bbb35ef756cf9cb15974d7097375d4f6086
Yash-patil03 <129077732+Yash-patil03@users.noreply.github.com>
2026-01-25 21:53:48 +0530

    Delete src/main directory

--- src/main/java/com/pgfinder/auth/dto/LoginDTO.java
+++
@@ -1,9 +0,0 @@
-package com.pgfinder.auth.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDTO {
-    private String email;
-    private String password;
-}
--- src/main/java/com/pgfinder/auth/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pgfinder.auth.repository;
-
-import com.pgfinder.auth.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Optional<Role> findByRoleName(String roleName);
-}
